107468a13c83738b67aea195a93c00d9b58a6037
Gercidio <gercidio@gmail.com>
2024-01-22 09:08:51 -0300

    rrd, components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Footer from './pages/footer/Footer'
-import Home from './pages/home/Home'
-import NavBar from './pages/navBar/NavBar'
+import Footer from './components/footer/Footer.tsx'
+import Navbar from './components/navBar/NavBar.tsx'
+import Home from './pages/home/Home.tsx'
+import Login from './pages/login/Login.tsx'
 
 function App() {
 
   return (
     <>
-      <NavBar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+      <Navbar/>
+      <div className='min-h-[80vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          
+
+        </Routes>
+
+      </div>
+      <Footer/>
+      </BrowserRouter>    
     </>
   )
 }
